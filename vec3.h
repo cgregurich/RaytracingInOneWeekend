0694be9f2ba615628627dd00fb53e26a346b848a
Colin Gregurich <coling112198@gmail.com>
2024-07-21 21:16:51 -0500

    understanding vec3

---
+++ vec3.h
@@ -0,0 +1,23 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <math>
+#include <iostream>
+
+using std::sqrt;
+
+class vec3 {
+    public:
+        double e[3];
+
+        vec3() : e{0,0,0} {}
+        vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+        double x() const { return e[0]; }
+        double y() const { return e[1]; }
+        double z() const { return e[2]; }
+
+        vec3 operator-() const { return vec3{-e[0], -e[1], -e[2]} }
+        double operator[](int i) const { return e[i]; }
+        double& operator[](int i) { return e[i]; }
+}
